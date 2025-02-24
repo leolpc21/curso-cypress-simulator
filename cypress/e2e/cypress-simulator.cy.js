4e0b6685d0d62d6c521b9e2d8f71ba073eb997e4
Leo Costa <leolpc21@hotmail.com>
2025-02-24 14:38:07 -0300

    feat: comando que exibe e esconde o botão de logout

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -11,19 +11,19 @@ describe('Cypress Simulator', () => {
 
   it('successfully simulates a Cypress command (e.g., cy.log("Yay!"))', () => {
     cy.submeterCodigo("cy.log('Yay!')", "Success:", "cy.log('Yay!') // Logged message 'Yay!'");
-  })
+  });
 
   it('shows an error when entering and running an invalid Cypress command (e.g., cy.run())', () => {
     cy.submeterCodigo('cy.run()', 'Error:', 'Invalid Cypress command: cy.run()');
-  })
+  });
 
   it('shows a warning when entering and running a not-implemented Cypress command (e.g., cy.contains("Login"))', () => {
     cy.submeterCodigo('cy.contains("Login")', 'Warning:', 'The `cy.contains` command has not been implemented yet.');
-  })
+  });
 
   it('shows an error when entering and running a valid Cypress command without parentheses (e.g., cy.visit)', () => {
     cy.submeterCodigo('cy.visit', 'Error:', 'Missing parentheses on `cy.visit` command');
-  })
+  });
 
   it('asks for help and gets common Cypress commands and examples with a link to the docs', () => {
     cy.submeterCodigo('help', 'Common Cypress commands and examples:', 'For more commands and details, visit the official Cypress API documentation.');
@@ -33,7 +33,7 @@ describe('Cypress Simulator', () => {
       .and('have.attr', 'target', '_blank')
       .and('have.attr', 'rel', 'noopener noreferrer')
       .and('be.visible');
-  })
+  });
 
   it('maximizes and minimizes a simulation result', () => {
     cy.submeterCodigo("cy.log('Yay!')", 'Success:', "cy.log('Yay!') // Logged message 'Yay!'");
@@ -54,7 +54,7 @@ describe('Cypress Simulator', () => {
       .click();
 
     cy.get('#expandIcon').should('be.visible');
-  })
+  });
 
   it('logs out successfully', () => {
     cy.get("#sandwich-menu").click();
@@ -62,49 +62,53 @@ describe('Cypress Simulator', () => {
 
     cy.get("#login").should("be.visible");
     cy.get("#sandwich-menu").should("not.be.visible");
-  })
+  });
 
-  it.skip('show and hide logout button', () => {
+  it('shows and hides the logout button', () => {
+    cy.get("#sandwich-menu").click();
+    cy.contains("#logoutButton", "Logout").should("be.visible");
 
-  })
+    cy.get("#sandwich-menu").click();
+    cy.contains("#logoutButton", "Logout").should("not.be.visible");
+  });
 
   it.skip('running... state', () => {
 
-  })
+  });
 
   it.skip('accept cookies', () => {
 
-  })
+  });
 
   it.skip('decline cookies', () => {
 
-  })
+  });
 
   it.skip('captcha button state', () => {
 
-  })
+  });
 
   it.skip('captcha error', () => {
 
-  })
+  });
 
   it.skip('run button - enable/disable states', () => {
 
-  })
+  });
 
   it.skip('reset textarea on logout and login', () => {
 
-  })
+  });
 
   it.skip('disabled run button on logout and login', () => {
 
-  })
+  });
 
   it.skip('reset output on logout and login', () => {
 
-  })
+  });
 
   it.skip('no cooking banner on the login page', () => {
 
-  })
-})
+  });
+});
