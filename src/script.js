85a344a488449291b2a4f666328d0bc3d67601e8
Leo Costa <leolpc21@hotmail.com>
2025-03-22 20:07:22 -0300

    Configure CI artifacts and break the app (git commit -m "Configure CI artifacts and break the app")

--- src/script.js
+++ src/script.js
@@ -573,7 +573,7 @@ For more commands and details, visit the <a href="https://docs.cypress.io/api/ta
 
       runButton.disabled = false
       runButton.classList.remove("loading")
-      runButton.innerHTML = "Runmmm"
+      runButton.innerHTML = "Rummn"
     }, (Math.floor(Math.random() * 5) + 1) * 1000)
   }
 })
